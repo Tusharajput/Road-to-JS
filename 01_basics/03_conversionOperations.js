021e0691345e8cb84951267d88f3ca256d37e51e
m_tushr <84866261+Tusharajput@users.noreply.github.com>
2023-10-07 15:00:06 +0000

    conversion of DataTypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,20 @@
+let score = 22
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/*
+"22" => 33
+"22abc" => Nan (not a number)
+"true" => 1 ; fasle => 0
+*/
+let isLoggedIn = 1
+let boolenisLoggedIn = Boolean(isLoggedIn)
+console.log(boolenisLoggedIn);
+
+let some
