d0e5e2d5b66cddcbb8c5c5a497342045e05604cc
m_tushr <84866261+Tusharajput@users.noreply.github.com>
2023-10-07 13:35:56 +0000

    variable & Naming convention

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,8 @@
+const accountId = 134
+let accountemail = "metushr@gmail.com"
+var accountPassword= "12345"
+accountCity = "delhi"
+
+// once the variable is defined using "const" it can't be changed
+
+console.log(accountId);
