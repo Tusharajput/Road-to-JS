3d430e3073570f88a5607f903c084301587d507d
m_tushr <84866261+Tusharajput@users.noreply.github.com>
2023-10-08 12:10:19 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,11 +1,11 @@
-const name = "hitesh"
+const name = "tushr"
 const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hitesh-hc-com')
+const gameName = new String('tushr-hc-com')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -22,11 +22,11 @@ console.log(newString);
 const anotherString = gameName.slice(-8, 4)
 console.log(anotherString);
 
-const newStringOne = "   hitesh    "
+const newStringOne = "   tushr    "
 console.log(newStringOne);
 console.log(newStringOne.trim());
 
-const url = "https://hitesh.com/hitesh%20choudhary"
+const url = "https://tushr.com/tushr%20choudhary"
 
 console.log(url.replace('%20', '-'))
 
