3758757fd06b3ef7a18d902808cf3d031451fb36
m_tushr <84866261+Tusharajput@users.noreply.github.com>
2023-10-08 16:59:01 +0000

    obj p-2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,55 @@
+// const spotifyUser = new Object()
+
+const spotifyUser = {}
+
+spotifyUser.id = "1234"
+spotifyUser.name = "tushr"
+spotifyUser.isLoggedIn = false
+
+// console.log(spotifyUser);
+
+const regularUser ={
+
+    email : "tushr@windows.com",
+    fullName : {
+        userFullName : {
+            FirstName : "tushar",
+            LastName : "rajput"
+        }
+    }
+}
+
+// console.log(regularUser.fullName.userFullName.FirstName);
+
+const obj1 = {1: "A",2 :"B", }
+const obj2 = {3: "A",4 :"D", }
+const obj3 = { ...obj1,...obj2}
+// console.log(obj3);
+
+const user =[
+    {
+        id : 1,
+        email : "t@google.com"
+    },
+    {
+        id : 1,
+        email : "t@google.com"
+    },
+    {
+        id : 1,
+        email : "t@google.com"
+    },
+    {
+        id : 1,
+        email : "t@google.com"
+    }
+]
+
+user[1].email
+console.log(spotifyUser);
+
+console.log(Object.keys(spotifyUser));
+console.log(Object.values(spotifyUser));
+console.log(Object.entries(spotifyUser));
+
+console.log(spotifyUser.hasOwnProperty('isLogged'));
