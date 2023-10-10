09b3fa76d31a53cc333953fd01c833522d8f31cf
m_tushr <84866261+Tusharajput@users.noreply.github.com>
2023-10-10 17:01:18 +0000

    control flow code files

---
+++ 04_control_flow.js/one.js
@@ -0,0 +1,46 @@
+// if
+const isUserloggedIn = true
+const temperature = 41
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// 
+const balance =1000
+
+// if (balance>500) console.log("test"),console.log("test-2");
+
+// if (balance < 500) {
+//     console.log("less than 5");
+// }else if (balance <750) {
+//     console.log("less than 750");
+// } 
+//  else if (balance <900) {
+//     console.log("less than 750");
+// } else{
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = true
+const debitcard = true
+const LoggedInFromGoogle = false
+const LoggedInFromEmail= true
+
+if (userLoggedIn && debitcard && 2==3 ) {
+    console.log("you can buy");
+}
+
+if (LoggedInFromGoogle || LoggedInFromEmail || true ) {
+    console.log("you can");
+}
