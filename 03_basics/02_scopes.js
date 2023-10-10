0ebeb049c1f2316fb20eaca2ee5cb1106c2f7af9
m_tushr <84866261+Tusharajput@users.noreply.github.com>
2023-10-10 13:05:49 +0000

    scope and this arrow

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,57 @@
+// var c = 300
+let a= 300
+
+if (true) {
+    let a = 23
+    const e = 66
+    // console.log("inner" , a);
+
+}
+
+
+// console.log(a);
+// console.log(e);
+// console.log(c);
+
+
+function one(){
+
+    const username =" tushr"
+
+    function two(){
+        const website = "yt"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+
+if (true) {
+    const username = "tushr"
+    if (username === "tushr") {
+        const web =   " stream"
+        // console.log(username +  web);
+    }
+    // console.log(web);
+}
+// console.log(username);
+
+//   +++++++++++++++++++++++++++++ interesrting ++++++++++++//\
+
+console.log(addOne(5))
+
+function addOne(num){
+    return num + 1
+}
+
+
+addtwo(5)
+
+const addtwo = function(num){
+    return num + 2
+}
+
