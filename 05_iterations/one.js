583b8da269b069f6cf854dd58fad2da19ca24474
m_tushr <84866261+Tusharajput@users.noreply.github.com>
2023-10-11 16:28:09 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,39 @@
+//for
+
+// for (let j = 0; j < array.length; j++) {
+//     const element = array[j];
+// }
+
+// for (let j = 0; j <= 10; j++) {
+//     const element = j;
+//     jf (element ==5) 
+//         console.log("5 js best number");
+//     }
+//     //console.log(element);
+    
+// }
+
+// for (let j = 0; j <= 10; j++) {
+//     console.log(`outer loop value : ${i}`);
+//     for (let j = 0; j <= 10 ; j++) {
+//         console.log(`outer loop value : ${j}`);
+//     }
+// }
+
+// break and continue
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(" 5 Detected");
+//         break
+//     }
+//     console.log(`value of i is ${index}`);  
+// }
+ 
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(" 5 Detected");
+        continue
+    }
+    console.log(`value of i is ${index}`);  
+}
